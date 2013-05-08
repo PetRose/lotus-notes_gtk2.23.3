10ad5867cc2ad753be663caed7edfaa04afa3b33
Søren Holm <sgh@sgh.dk>
2013-05-08 10:57:43 +0200

    Remove temporary dir after fixing dependencies.

--- fix-8.5.2-dependencies.sh
+++ fix-8.5.2-dependencies.sh
@@ -1,3 +1,4 @@
 dpkg-deb -R ibm-lotus-notes-8.5.2.i586.deb ibm-lotus-notes-8.5.2.i586
 cp ibm-lotus-notes-8.5.2.i586_control ibm-lotus-notes-8.5.2.i586/DEBIAN/control
 dpkg-deb -b ibm-lotus-notes-8.5.2.i586 ibm-lotus-notes-8.5.2.i586-fixed.deb
+rm -rf ibm-lotus-notes-8.5.2.i586
